8f0d5401ab610436e5deb26cb2514630984126aa
Yordanos Shanbel <yordanosshanbel28@gmail.com>
2025-11-12 19:59:07 +0300

    Add axios dependency and implement extension card component with dynamic data fetching

--- src/components/ExtentionCard.jsx
+++ src/components/ExtentionCard.jsx
@@ -2,13 +2,58 @@
 //         "name": "DevLens",
 //         "description": "Quickly inspect page layouts and visualize element boundaries.",
 //         "isActive": true
-import { useState } from "react";
+import { useEffect, useState } from "react";
 
 function ExtentionCard() {
   const [isOn, setIsOn] = useState(false);
+  const [extensionList, setExtensionList] = useState([]);
+
+  async function fetchData() {
+    try {
+      const response = await fetch("../../data.json");
+      const data = await response.json();
+      if (data) {
+        setExtensionList(data);
+      }
+      console.log(data);
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <>
-      <div className="extension-card">
+      {" "}
+      {extensionList.map((extension) => {
+        return (
+          <div key={extension.name} className="extension-card">
+            <div className="top-part">
+              <img src={`./src/${extension.logo}`} alt="" />
+              <div className="extension-body">
+                <h1 className="extension-header">{extension.name}</h1>
+                <p className="extesion-description">{extension.description}</p>
+              </div>
+            </div>
+
+            <div className="bottom-part">
+              <button className=" btn remove-btn">Remove</button>
+              <label className="switch">
+                <input
+                  type="checkbox"
+                  checked={extension.isActive}
+                  onChange={() => setIsOn(!isOn)}
+                />
+                <span className="slider" />
+              </label>
+            </div>
+          </div>
+        );
+      })}
+      {/* <div className="extension-card">
         <div className="top-part">
           <img src="./src/assets/images/logo-devlens.svg" alt="there" />
           <div className="extension-body">
@@ -30,7 +75,7 @@ function ExtentionCard() {
             <span className="slider" />
           </label>
         </div>
-      </div>
+      </div> */}
     </>
   );
 }
